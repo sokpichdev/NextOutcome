f296c691a33ab0b2853b3033d639f14b948f258c
Sok Pich <124275918+sokpichdev@users.noreply.github.com>
2026-06-29 00:17:47 +0700

    feat: created project
    -  nothing done yet

---
+++ NextOutcome/NextOutcomeTests/NextOutcomeTests.swift
@@ -0,0 +1,18 @@
+//
+//  NextOutcomeTests.swift
+//  NextOutcomeTests
+//
+//  Created by Sok Pich on 28/06/2026.
+//
+
+import Testing
+
+struct NextOutcomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
